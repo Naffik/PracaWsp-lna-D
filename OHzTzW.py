51d1dcc87322bd20d5c9fd9f0ea17f23b4c7eb98
Naffik <76112481+Naffik@users.noreply.github.com>
2022-01-29 20:03:02 +0100

    Update OHzTzW.py

--- OHzTzW.py
+++ OHzTzW.py
@@ -8,22 +8,21 @@ sns.set()
 t = np.linspace(0, 50, 1000)
 x = [0, 0]
 delta = 0.05
-omega = 2
 k = 2
 m = 2
+omega = math.sqrt(k/m)
 A = 0.1
 
 
 def sho(t, x):
-    result = [x[1], (-2 * delta * x[1] - (k / m) * x[0] + A * math.sin(omega*t))]
+    result = [x[1], (-2 * delta * x[1] - (k / m) * x[0] + A * math.sin(1*omega*t))]
     return result
 
 
 solution = solve_ivp(sho, [0, 1000], y0=x, t_eval=t)
 plt.plot(t, solution.y[0], 'b')
-plt.axis([0, 50, -0.075, 0.075])
-plt.xlabel("Czas")
-plt.ylabel("Wychylenie")
+# plt.xlabel("Czas")
+# plt.ylabel("Wychylenie")
 plt.title('Oscylator z tłumieniem i z wymuszeniem', fontsize=20)
 plt.grid(True)
 plt.show()
